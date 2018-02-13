57b911e7d4bb7a17be8ec6e0edb1846b86b75f79
hansel.tritama <hanselgunawan94@gmail.com>
2018-02-13 00:38:20 -0800

    haha

--- client/components/Add.js
+++ client/components/Add.js
@@ -159,8 +159,15 @@ class Add extends Component {
     fetchLocationDetails = (suggest,key) => {
         if(suggest)
         {
-            console.log("https://maps.googleapis.com/maps/api/place/details/json?placeid="+suggest.placeId+"&key=AIzaSyAeToo6vapEqsV-wm4UcDLaA0CwiVEHpVs");
-            axios.get("https://maps.googleapis.com/maps/api/place/details/json?placeid="+suggest.placeId+"&key=AIzaSyAeToo6vapEqsV-wm4UcDLaA0CwiVEHpVs")
+            console.log("https://maps.googleapis.com/maps/api/place/details/json?placeid="+suggest.placeId+"&key=AIzaSyAZ-CwVxjEd2hGyIKbXzgx9A7ZdowjuYFI");
+            axios.get("https://maps.googleapis.com/maps/api/place/details/json?placeid="+suggest.placeId+"&key=AIzaSyAZ-CwVxjEd2hGyIKbXzgx9A7ZdowjuYFI", {
+                headers: {
+                    'Access-Control-Allow-Origin': 'https://dry-chamber-76409.herokuapp.com',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authentication': ''
+                }
+            })
                 .then(res => {
                     this.setState({geolocation:res.data.result});
                     let myArr = this.state.travelList;
